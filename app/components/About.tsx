d5478e3437a2f9b23ff641d46ff53a737af0b6ea
Vedant-Rasote <vedant.d.rasote@gmail.com>
2024-01-21 15:44:42 +0530

    Added place holder components, Added abouts content

---
+++ app/components/About.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Gowun_Batang } from 'next/font/google';
+import Link from 'next/link';
+const gowunBatang = Gowun_Batang({ subsets: ['latin'], weight: '400' });
+import { Parallax, Background } from 'react-parallax';
+import Image from 'next/image';
+
+const About = () => {
+    return (
+        <div className={`${gowunBatang.className} container`}>
+            <div className="md:p-10 md:flex lg:flex">
+                <div className="sm:w-full md:w-1/2 flex flex-col md:p-10 justify-center">
+                    <h2 className='uppercase text-lg md:text-xl lg:text-2xl my-4 accent'>About</h2>
+                    <h1 className='text-4xl my-3'>Prize winning Mediterreanean cuisine</h1>
+                    <p className='text-xl my-3'>We are a family-owned Mediterranean restaurant located in Chicago, Illinois. Our restaurant is known for its focus on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish cultures to create a unique fusion of flavors.</p>
+                    <div className="flex my-4 space-x-3">
+                        <Link href='' className='inline hover-bg-accent p-2 border-2'>
+                            <p>Reserve</p>
+                        </Link>
+                        <Link href='' className='inline hover-bg-accent p-2 border-2'>
+                            <p>Go to Menu</p>
+                        </Link>
+                    </div>
+                </div>
+                <div className="sm:w-full md:w-1/2 flex flex-col md:p-10">
+                    <div className='hidden md:block'>
+                        <Parallax bgImage='about.webp' strength={300}>
+                            <div style={{ height: '500px' }} />
+                        </Parallax>
+                    </div>
+                    <div className='block md:hidden'>
+                        <Parallax bgImage='about.webp' strength={200}>
+                            <div style={{ height: '300px' }} />
+                        </Parallax>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default About
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+import About from "./components/About";
+import Awards from "./components/Awards";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
 import Hero from "./components/Hero";
+import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import Section from "./components/Section";
+import Testimonials from "./components/Testimonials";
 
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <About />
+      <Awards />
+      <Gallery />
+      <Menu />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   );
 }

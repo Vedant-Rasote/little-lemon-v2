274be28f5b9d2dbf70216ca1ef67c9446cab5733
Vedant-Rasote <vedant.d.rasote@gmail.com>
2024-01-21 15:54:47 +0530

    Added awards section

--- app/components/Awards.tsx
+++ app/components/Awards.tsx
@@ -1,6 +1,20 @@
+'use client'
+import Image from 'next/image';
+import Marquee from "react-fast-marquee";
+
 const Awards = () => {
   return (
-    <div>Awards</div>
+    <Marquee className='border-y border-[#5b564a] my-10 md:my-0 py-10'>
+      <Image className='mx-10' width={100} height={100} src='/1.webp' alt='Award 1' />
+      <Image className='mx-10' width={100} height={100} src='/2.webp' alt='Award 2' />
+      <Image className='mx-10' width={100} height={100} src='/3.webp' alt='Award 3' />
+      <Image className='mx-10' width={100} height={100} src='/4.webp' alt='Award 4' />
+      <Image className='mx-10' width={100} height={100} src='/5.webp' alt='Award 5' />
+      <Image className='mx-10' width={100} height={100} src='/6.webp' alt='Award 6' />
+      <Image className='mx-10' width={100} height={100} src='/7.webp' alt='Award 7' />
+      <Image className='mx-10' width={100} height={100} src='/8.webp' alt='Award 8' />
+      <Image className='mx-10' width={100} height={100} src='/9.webp' alt='Award 9' />
+    </Marquee>
   )
 }
 
